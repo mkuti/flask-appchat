abca1d11d6fcde165feaca2aed4a3aa5a4c0dfb7
mkuti <pionste@gmail.com>
2020-01-05 20:58:48 +0000

    adding run.py

--- run.py
+++ run.py
@@ -1,13 +1,30 @@
 import os
-from flask import Flask, render_template
-
+from flask import Flask
 
 app = Flask(__name__)
+messages = []
 
 
-@app.route('/')
+def add_messages(username, message):
+    messages.append("{o}: {1}".format(username, message))
+
+
+app.route("/")
 def index():
-    return "<h1>Hello there</h1>"
+    ''' Main page with instructions '''
+    return "To send a message use /USERNAME/MESSAGE"
 
 
-app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
+app.route("/<username>")
+def user(username):
+    ''' Display chat messages '''
+    return "Welcome, {}".format(username)
+
+
+app.route("/<username>/<message")
+def send_message(username, message):
+    """Create a new message and redirect back to the chat page"""
+    return "{o}: {1}".format(username, message)
+
+
+app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
