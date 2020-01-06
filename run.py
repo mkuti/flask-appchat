9e0e5566368d8628d4d6512d3613ac6aa8095d5a
mkuti <pionste@gmail.com>
2020-01-06 23:41:28 +0000

    added javascript timeout function and textbox

--- run.py
+++ run.py
@@ -1,45 +1,41 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, url_for
+
 
 app = Flask(__name__)
 app.secret_key = "randomstring123"
 messages = []
 
 
-def add_messages(username, message):
+def add_message(username, message):
     """Add messages to the `messages` list"""
     now = datetime.now().strftime("%H:%M:%S")
-    messages.append("({}) {}: {}".format(now, username.capitalize(), message))
-
-
-def get_all_messages():
-    """Get all of the messages and separate them with a `br`"""
-    return "<br>".join(messages)
-
-
+    messages.append({"timestamp": now, "from": username, "message": message})
+ 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    """ Main page with form to enter username """
-
+    """Main page with instructions"""
     if request.method == "POST":
-        session["username"] = request.form["username"] #username name filled in the form
+        session["username"] = request.form["username"]
 
     if "username" in session:
-        return redirect(session["username"]) #redirect to username route and because data is stored, home page will stay as logged in until we close browser
+        return redirect(url_for("user", username=session["username"]))
 
     return render_template("index.html")
 
 
-@app.route("/<username>")
+@app.route("/chat/<username>", methods=["GET", "POST"])
 def user(username):
-    return "<h1>Welcome {0} </h1>{1}".format(username.capitalize(), get_all_messages())
+    """Display chat messages"""
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_message(username, message)
+        return redirect(url_for("user", username=session["username"]))
 
-
-@app.route("/<username>/<message>")
-def send_message(username, message):
-    add_messages(username, message)
-    return redirect("/" + username)
+    return render_template("chat.html", username=username,
+                           chat_messages=messages)
 
 
 app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", "5000")), debug=True)
